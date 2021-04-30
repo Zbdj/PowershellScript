189883a0c55f6565339af80975a269191e03ed1e
Zbdj <45045776+Zbdj@users.noreply.github.com>
2021-04-30 15:57:55 +0200

    Update and rename saveData.ps1 to P7_Zeboudj_Script04.ps1

--- P7_Zeboudj_Script04.ps1
+++ P7_Zeboudj_Script04.ps1
@@ -1,4 +1,4 @@
-﻿#===========================================================================================#
+#===========================================================================================#
 # Usage : Ce script permet de sauvegarder les dossiers de l'utilisateur sur le serveur      #
 # Cmd : Le script est lancé avec le plannificateur de tâches                                #
 # Version : 1.4                                                                             #
@@ -7,12 +7,13 @@
 
 #Le script récupere le nom de la session actuellement en cours
 $session = $env:USERNAME
+$pc = $env:COMPUTERNAME
 
 function checkFolder {
     #Test-Path vérifie si un dossier au nom de l'utilisateur sur le serveur existe
-    if((Test-Path -Path \\Win-fqm9kj6kb7n\SAV\$session) -eq $false){
+    if((Test-Path -Path \\Win-fqm9kj6kb7n\SAV\$pc) -eq $false){
         #Si le dossier n'existe pas, cette commande le crée
-        New-Item -ItemType directory -Path \\Win-fqm9kj6kb7n\SAV\$session | Out-Null
+        New-Item -ItemType directory -Path \\Win-fqm9kj6kb7n\SAV\$pc | Out-Null
     }
 
     #Appel de la fonction saveData qui permet d'enregistrer tous les documents
@@ -21,14 +22,12 @@ function checkFolder {
 
 function saveData {
         #Boucle sur les dossiers de l'utilisateur afin de récupérer le nom des fichiers/dossier qui ne sont pas cachés
-         ForEach ($Files in (Get-ChildItem -Path "C:\Users\$session\" -Name -Recurse)) {
-            #Robocopy copie les données d’un fichier d’un emplacement à un autre.
-                #/SEC copie les securités ACL / NTFS
-                #/MIR copie tous les sous répertoires + supprime les fichiers qui n'existe plus
-                #/NFl /NDL indique que les noms de fichier/répertoire ne doivent pas être consignés. (?:*<>)
-            robocopy "C:\Users\$session\$Files" "\\Win-fqm9kj6kb7n\SAV\$session\$Files" /COPYALL /SEC /MIR /R:0 /W:0 /NFL /NDL
-            #Les fichiers enfants hériste des autorisations du dossier parent
-            icacls "\\Win-fqm9kj6kb7n\SAV\$session\$Files" /reset /t /c
+         ForEach ($Files in (Get-ChildItem -Path "C:\Users" -Name -Recurse)) {
+         if($Files -notmatch "Public"){
+          #Robocopy copie les données d’un fichier d’un emplacement à un autre.
+          robocopy "C:\Users\$Files" "\\Win-fqm9kj6kb7n\SAV\$pc\$Files" /COPYALL
+        }
+     
       }
         return 0
 }
