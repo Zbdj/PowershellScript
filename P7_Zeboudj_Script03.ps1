568f6048a1dc44053d1940b1a1c471385583586f
Zbdj <45045776+Zbdj@users.noreply.github.com>
2021-04-30 15:54:00 +0200

    Update and rename GetGroupeAd.ps1 to P7_Zeboudj_Script03.ps1

--- P7_Zeboudj_Script03.ps1
+++ P7_Zeboudj_Script03.ps1
@@ -1,4 +1,4 @@
-﻿#========================================================================================#
+#========================================================================================#
 # Usage : Ce script permet de lister les groupes dans lesquel l'utilisateur est membre   #
 # Cmd : Interactif ou silencieux avec => powershell ./GetGroupAd.ps1 -Username String    #
 # Version : 1.8                                                                          #
@@ -19,10 +19,8 @@ function GetGroup {
 
     #Si l'utilisateur n'existe pas, le script s'arrête, il y a la possibilité de le relancer en tapant "o"
     if($UserExist -eq $null){
-        $Retry = Read-Host "`nL'utilisateur $Username n'existe pas dans l'AD, souhaitez vous relancer une recherche? (OUI = o / NON = n)"
-            if($Retry -eq 'o'){
-                GetGroup
-            }
+        Write-Host "`nL'utilisateur $Username n'existe pas dans l'AD, veuillez réessayer..."
+        return 1
     } else {
             #Cette commande récupere le nom des groupes dans lesquel l'utilisateur est membre
             $Response = Get-ADPrincipalGroupMembership -Identity $Username | select name | Out-String
